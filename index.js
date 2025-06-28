86a736803dcc0e1ddf280a436052ef3e3b8482d3
jasmine-singh25 <jasmine.work2505@gmail.com>
2025-06-29 03:39:58 +0530

    menu added
    menu option added in all files

--- index.js
+++ index.js
@@ -48,3 +48,10 @@ reportsRef.on('value', snapshot => {
     });
     displayReports(reports);
 });
+ const sidebar = document.getElementById('sidebar');
+ const toggleBtn = document.getElementById('toggle-btn');
+
+ toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('collapsed');
+  console.log('Toggle button clicked');
+ });
--- signup.js
+++ signup.js
@@ -61,3 +61,10 @@
         alert(error.message);
       });
   });
+  const sidebar = document.getElementById('sidebar');
+ const toggleBtn = document.getElementById('toggle-btn');
+
+ toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('collapsed');
+  console.log('Toggle button clicked');
+ });
