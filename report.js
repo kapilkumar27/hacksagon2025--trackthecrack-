d58424a039e66aae09f12033edf8a36431ba94cc
jasmine-singh25 <jasmine.work2505@gmail.com>
2025-06-29 03:27:01 +0530

    menu button added
    menu button added on report page to close and open sidebar

--- report.js
+++ report.js
@@ -68,5 +68,10 @@ function submitReport(location, hazard, description, imageUrl) {
     reportForm.reset();
   });
 }
+ const sidebar = document.getElementById('sidebar');
+ const toggleBtn = document.getElementById('toggle-btn');
 
-
+ toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('collapsed');
+  console.log('Toggle button clicked');
+ });
